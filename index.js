0e1c63f7322106966c3e49404654920aef0dc629
Ramiro Meneses <rampratts@gmail.com>
2020-09-05 20:17:10 -0300

    Fix de rutas

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
 }
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
   
 app.use('/api/users', require('./routes/users'));
 
