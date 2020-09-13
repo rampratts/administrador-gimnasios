dee4263c056a036e08fd541f621ddbb04916886c
Ramiro Meneses <rampratts@gmail.com>
2020-09-13 16:20:23 -0300

    Comienzo de agregar clases

--- client/src/views/Clases.js
+++ client/src/views/Clases.js
@@ -3,11 +3,13 @@ import { Container, Row, Col } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
 import ListaClases from "../components/clases/ListaClases";
+import AgregarClase from "../components/clases/AgregarClase";
 
 const Clases = () => (
   <Container fluid className="main-content-container px-4">
-    <Row noGutters className="page-header py-4">
+    <Row noGutters className="flex-column flex-lg-row justify-content-end no-gutters page-header py-4 row">
       <PageTitle title="Clases" md="12" className="ml-sm-auto mr-sm-auto" />
+      <AgregarClase/>
     </Row>
     <Row>
         <Col lg="9">
