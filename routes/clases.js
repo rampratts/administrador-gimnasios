cded0351a9731ab206b8a75a5acd9877ee57984f
Fefo <feffee22@gmail.com>
2020-09-14 15:04:26 -0300

    Entrega2/registar-obtenerClases-fix

--- routes/clases.js
+++ routes/clases.js
@@ -7,41 +7,34 @@ const Auth = require('../middleware/authentication');
 const { route } = require('./users');
 
 
-
-router.post('/register', Auth.isAuth, Auth.isProf, async(req,res)=>{
+router.post('/register', Auth.isAuth, Auth.isProfOrAdmin, async(req,res)=>{
     
-    const { nombre, descripcion, horario,profesor } = req.body;
+    const { nombre, descripcion, horario, s, m, t, w, th, f, st, profesor } = req.body;
    
     try {
-    const claseid=uuidv4();
-    const clienteclaseid=uuidv4();
-    const { clienteid } = (await pool.query('SELECT id FROM cliente WHERE id_usuario = $1', [user.id])).rows[0];    
-    await pool.query("INSERT INTO clases VALUES($1, $2, $3, $4, $5)", [claseid,nombre,descripcion,horario,profesor]);            
-    await pool.query("INSERT INTO cliente_clases VALUES($1, $2, $3)", [clienteclaseid,clienteid,claseid,]);                      
-            
-    res.send({
-        status: "OK",
-        statusCode: 200,
-        results: "lesson created"
+        const claseid=uuidv4();
+        await pool.query("INSERT INTO clases VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)", [claseid,nombre,descripcion,horario,s,m,t,w,th,f,st,profesor]);            
+        res.send({
+            status: "OK",
+            statusCode: 200,
+            results: "lesson created"
     });
-
-    } catch (error) {
-    res.status(400).send(error);
-    }
-})
-
-
-router.get('/get', Auth.isAuth, async(req,res)=>{
-   try{
-    
-    const clases = [{ clase }] = await pool.query('SELECT id,nombre,descripcion,horario,profesor FROM clases as cl,');
-    if(!clases){
-        return res.status(200).send({error: 'No hay clases disponibles'});
-    }
-    
     } catch (error) {
         res.status(400).send(error);
     }
+})
+
+router.get('/get', Auth.isAuth, async(req,res)=>{
+    try{
+        const clases = await pool.query('SELECT id,nombre,descripcion,horario,s,m,t,w,th,f,st,profesor FROM clases');
+        if(!clases){
+           return res.status(200).send({error: 'No hay clases disponibles',});
+        }
+        res.send(clases);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
 })
 
 module.exports = router;
