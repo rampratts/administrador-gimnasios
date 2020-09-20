e90a9a674c822eccec11bda7a318b92c2524d15f
Fernandobolso <42076386+Fernandobolso@users.noreply.github.com>
2020-09-20 12:16:15 -0300

    archivopagos

--- routes/pagos.js
+++ routes/pagos.js
@@ -4,4 +4,4 @@ const { v4: uuidv4 } = require('uuid');
 const { check, body, validationResult } = require('express-validator');
 const pool = require('../db/db');
 const Auth = require('../middleware/authentication');
-const { route } = require('./users');
+module.exports = router;
