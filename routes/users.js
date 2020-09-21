13ea5be0b5296a20251ed9691c57e88272714a4e
Fernandobolso <42076386+Fernandobolso@users.noreply.github.com>
2020-09-21 18:13:11 -0300

    unidad3rutaclien

--- routes/users.js
+++ routes/users.js
@@ -154,7 +154,7 @@ router.get('/profesores', Auth.isAuth, async(req,res) => {
     }
 })
 
-router.get('/clientes', Auth.isAuth, async(req,res)=>{
+router.get('/clientes', Auth.isAuth, Auth.isProfOrAdmin, async(req,res)=>{
     try {
         const {gimnasio_id} = (await pool.query('SELECT gimnasio_id FROM usuario WHERE id = $1', [req.user.id])).rows[0];
         const clientes = (await pool.query('SELECT cliente.id, usuario.nombre, usuario.apellido, pago_mensual, deuda FROM cliente INNER JOIN usuario ON usuario_id = usuario.id WHERE usuario.gimnasio_id = $1', [gimnasio_id])).rows;
