7445223547cdca0da4c3aa0c3c410b1c2016aaad
Ramiro Meneses <rampratts@gmail.com>
2020-10-05 10:30:01 -0300

    Formateo de queries

--- db/db.sql
+++ db/db.sql
@@ -78,27 +78,58 @@ CREATE TABLE pago (
 
 --Esta view devuelve todos los usuarios con informacion sobre su tipo
 CREATE VIEW usuarios AS
-SELECT usuario.id, nombre_usuario, usuario.nombre, apellido, tipo_usuario, gimnasio.nombre AS gimnasio
+SELECT 
+	usuario.id,
+	nombre_usuario, 
+	usuario.nombre, 
+	apellido, 
+	tipo_usuario, 
+	gimnasio.nombre AS gimnasio
 FROM usuario 
 INNER JOIN gimnasio ON gimnasio.id = gimnasio_id;
 
 --Devuelve informacion sobre los administrativos
 CREATE VIEW administrativos AS
-SELECT administrativo.id AS admin_id, usuario.id AS usuario_id, usuario.nombre_usuario, usuario.nombre, usuario.apellido, administrativo.area, gimnasio.nombre AS gimnasio
+SELECT 
+	administrativo.id AS admin_id, 
+	usuario.id AS usuario_id, 
+	usuario.nombre_usuario, 
+	usuario.nombre, 
+	usuario.apellido, 
+	administrativo.area, 
+	gimnasio.nombre AS gimnasio
 FROM administrativo
 INNER JOIN usuario ON usuario_id = usuario.id
 INNER JOIN gimnasio on usuario.gimnasio_id = gimnasio.id;
 
 --Devuelve informacion sobre los profesores
 CREATE VIEW profesores AS
-SELECT profesor.id AS prof_id, usuario.id AS usuario_id, usuario.nombre_usuario, usuario.nombre, usuario.apellido, profesor.area, profesor.calificado, gimnasio.nombre AS gimnasio
+SELECT 
+	profesor.id AS prof_id, 
+	usuario.id AS usuario_id, 
+	usuario.nombre_usuario, 
+	usuario.nombre, 
+	usuario.apellido, 
+	profesor.area, 
+	profesor.calificado, 
+	gimnasio.nombre AS gimnasio
 FROM profesor
 INNER JOIN usuario ON usuario_id = usuario.id
 INNER JOIN gimnasio on usuario.gimnasio_id = gimnasio.id;
 
 --Devuelve informacion sobre los clientes
 CREATE VIEW clientes AS
-SELECT cliente.id AS clilente_id, usuario.id AS usuario_id, usuario.nombre_usuario, usuario.nombre, usuario.apellido, cliente.pago_mensual, cliente.deuda, cliente.peso_actual, cliente.imc, gimnasio.nombre AS gimnasio
+SELECT 
+	cliente.id AS clilente_id, 
+	usuario.id AS usuario_id, 
+	usuario.nombre_usuario, 
+	usuario.nombre, 
+	usuario.apellido, 
+	cliente.pago_mensual, 
+	cliente.deuda, 
+	cliente.peso_actual, 
+	cliente.imc, 
+	gimnasio.nombre AS gimnasio
 FROM cliente
 INNER JOIN usuario ON usuario_id = usuario.id
 INNER JOIN gimnasio on usuario.gimnasio_id = gimnasio.id;
