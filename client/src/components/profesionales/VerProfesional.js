7187ca534d3aa49a83797983c583e1a09fde9311
Ramiro Meneses <rampratts@gmail.com>
2020-11-13 14:52:20 -0300

    Cambiado header

--- client/src/components/profesionales/VerProfesional.js
+++ client/src/components/profesionales/VerProfesional.js
@@ -4,7 +4,7 @@ import { Col, Row, Modal, ModalHeader, ModalBody } from "shards-react";
 const VerProfesional = ({isOpen, toggle, profesional}) => {
   return (
     <Modal open={isOpen} toggle={toggle}>
-      <ModalHeader>Progreso</ModalHeader>
+      <ModalHeader>Profesional</ModalHeader>
       <ModalBody>
         <Row>
           <Col>
